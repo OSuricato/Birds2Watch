33e0531c7c378dbd05e870dea2e1ba4b59217f72
Victor Barbosa <barbosavictor30@gmail.com>
2025-01-28 09:29:09 -0300

    db adapter setup

--- .history/app/adapters/database_20250128092820.py
+++ .history/app/adapters/database_20250128092820.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
 from sqlalchemy.ext.declarative import declarative_base
 
+
 engine = create_engine(settings.DATABASE_URL, echo=True)  # echo=True for debug logs
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
